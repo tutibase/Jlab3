17832150624d5cc8056ffbfced44beb9991558cb
tutibase <dykun1784@gmail.com>
2024-12-11 19:53:18 +0300

    refactor: removing an unused isAvailable method

--- src/main/java/org/example/Pump.java
+++ src/main/java/org/example/Pump.java
@@ -1,23 +1,16 @@
 package org.example;
 
 public class Pump {
-    private boolean available = true;
     private final int pumpId;
 
     public Pump(int pumpId) {
         this.pumpId = pumpId;
     }
 
-    public synchronized boolean isAvailable() {
-        return available;
-    }
-
     public synchronized void startPumping(int fuelAmount) throws InterruptedException {
-        available = false;
         System.out.println(Thread.currentThread().getName() + " начал заправлять " + fuelAmount + " единиц топлива на колонке " + pumpId);
         Thread.sleep(1000); // ждем окончания заправки
         System.out.println(Thread.currentThread().getName() + " закончил заправляться");
-        available = true;
         notifyAll();
     }
 }
