3d48cbf26a53ec01ee892b5d0575530f832477d5
tutibase <dykun1784@gmail.com>
2024-12-12 15:58:06 +0300

    refactor: removing an unused notifyAll

--- src/main/java/org/example/Pump.java
+++ src/main/java/org/example/Pump.java
@@ -11,6 +11,5 @@ public class Pump {
         System.out.println(Thread.currentThread().getName() + " начал заправлять " + fuelAmount + " единиц топлива на колонке " + pumpId);
         Thread.sleep(1000); // ждем окончания заправки
         System.out.println(Thread.currentThread().getName() + " закончил заправляться");
-        notifyAll();
     }
 }
