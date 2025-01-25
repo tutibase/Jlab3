5ace63ec90e30efc9a89711434616aa006d40175
tutibase <dykun1784@gmail.com>
2025-01-25 14:45:42 +0300

    feat: totalMoney on Cashier

--- src/main/java/org/example/Cashier.java
+++ src/main/java/org/example/Cashier.java
@@ -5,16 +5,20 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class Cashier {
     private final ArrayBlockingQueue<Pump> availablePumps;
+    private int totalMoney;
 
     public Cashier(Pump[] pumps) {
         this.availablePumps = new ArrayBlockingQueue<>(3);
         availablePumps.addAll(Arrays.asList(pumps));
+        this.totalMoney = 0;
     }
 
     public synchronized void payForFuel(int fuelAmount) throws InterruptedException {
-        System.out.println(Thread.currentThread().getName() + " платит за " + fuelAmount + " единиц топлива");
-        Thread.sleep(100); // ждем окончания оплаты
-        System.out.println(Thread.currentThread().getName() + " окончил оплату");
+        int payment = fuelAmount * 10;
+        System.out.println(Thread.currentThread().getName() + " платит " + payment + " за " + fuelAmount + " единиц топлива");
+        Thread.sleep(100);
+        totalMoney += payment;
+        System.out.println(Thread.currentThread().getName() + " окончил оплату. Сейчас в кассе " + totalMoney);
     }
 
     public Pump getAvailablePump() throws InterruptedException {
