83792dbb0adb2ca1d6694c862735a7bf4cd4527b
tutibase <dykun1784@gmail.com>
2025-01-25 15:17:00 +0300

    feat: fuel transfer in the thread

--- src/main/java/org/example/Pump.java
+++ src/main/java/org/example/Pump.java
@@ -1,15 +1,34 @@
 package org.example;
 
-public class Pump {
+public class Pump implements Runnable {
     private final int pumpId;
+    private int fuelAmount;
 
     public Pump(int pumpId) {
         this.pumpId = pumpId;
     }
 
-    public synchronized void startPumping(int fuelAmount) throws InterruptedException {
+    public void startPumping(int fuelAmount) throws InterruptedException {
+        this.fuelAmount = fuelAmount;
         System.out.println(Thread.currentThread().getName() + " начал заправлять " + fuelAmount + " единиц топлива на колонке " + pumpId);
-        Thread.sleep(1000); // ждем окончания заправки
+
+        Thread pumpingThread = new Thread(this);
+        pumpingThread.start();
+        pumpingThread.join();
+
         System.out.println(Thread.currentThread().getName() + " закончил заправляться");
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < fuelAmount; i++) {
+                Thread.sleep(100); // Имитация передачи топлива
+                //System.out.println(Thread.currentThread().getName() + " передал 1 единицу топлива на колонке " + pumpId);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
