08f2d9b709ad65ff084ef68995981df7eeb87439
tutibase <dykun1784@gmail.com>
2025-01-25 14:33:57 +0300

    feat: customersThread added

--- src/main/java/org/example/GasStation.java
+++ src/main/java/org/example/GasStation.java
@@ -8,20 +8,15 @@ public class GasStation {
         Cashier cashier = new Cashier(pumps);
         Random rand = new Random();
 
-        Thread[] customers = new Thread[7];
-        for (int i = 0; i < customers.length; i++) {
-            customers[i] = new Thread(new Customer(cashier, rand.nextInt(50) + 1));
-            customers[i].start();
-        }
+        // Создаем поток клиентов
+        Thread customerThread = new Thread(() -> {
+            while (true) {
+                int fuelAmount = rand.nextInt(50) + 1;
+                Customer customer = new Customer(cashier, fuelAmount);
+                new Thread(customer).start();
 
-        for (Thread customer : customers) {
-            try {
-                customer.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
             }
-        }
-
-        System.out.println("Все покупатели закончили");
+        });
+        customerThread.start();
     }
 }
